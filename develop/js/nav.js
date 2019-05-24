45bea2d2314ff1f8d330fcb04633d280bb0f7f39
Andrey Obzhigalov <obzhigalov.andrew@gmail.com>
2019-05-24 18:41:38 +0300

    сделал меню на маленьких устройствах

---
+++ develop/js/nav.js
@@ -0,0 +1,34 @@
+let menu = function(elems) {
+    let burger = document.querySelector(elems.burger);
+    let nav = document.querySelector(elems.nav);
+    let link = document.querySelector(elems.link);
+    let navlist = document.querySelector(elems.navlist);
+
+    let _togglenav = function() {
+        burger.classList.toggle('burger--active');
+        nav.classList.toggle('nav--active');
+    };
+
+    let menuevent = function() {
+        burger.addEventListener('click', _togglenav);
+        navlist.addEventListener('click', function(click){
+            target = click.target;
+
+            if (target.classList.contains('nav-list__link')) {
+                _togglenav();
+            }
+        })
+    };
+
+    return {
+        navexist: menuevent
+    }
+}({
+    burger: '.burger',
+    nav: '.nav',
+    navlist: '.nav-list',
+    link:'.nav-list__link'
+});
+
+
+menu.navexist();
