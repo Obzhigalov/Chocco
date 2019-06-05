6522bd16381cd36ba4f3606ba1aebf5a241df743
Andrey Obzhigalov <obzhigalov.andrew@gmail.com>
2019-06-05 16:46:45 +0300

    сделал опс

--- develop/js/ops.js
+++ develop/js/ops.js
@@ -92,18 +92,3 @@ let ops = function() {
 ops();
 var xStart, yStart = 0; 
 
-document.addEventListener('touchstart', function(e) {
-    xStart = e.touches[0].screenX;
-    yStart = e.touches[0].screenY;
-    console.log("hui")
-}); 
-
-document.addEventListener('touchmove', function(e) {
-    var xMovement = Math.abs(e.touches[0].screenX - xStart);
-    var yMovement = Math.abs(e.touches[0].screenY - yStart);
-    if((yMovement * 3) > xMovement) {
-        e.preventDefault();
-    }
-});
-
-
